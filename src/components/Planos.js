56f9c883013036ce4a41c74bf2507217478851cf
Alysson Teixeira <queijos410@gmail.com>
2021-10-12 16:10:31 -0300

    planos design

--- src/components/Planos.js
+++ src/components/Planos.js
@@ -53,6 +53,61 @@ function Planos() {
           <nobr> R$ 200,00 </nobr> 
           </div>
         </div>
+        <div id='Principais3'>
+          <div className='planos1'>   
+            <div className='Cursos2'>
+              Pratico I <br /><br />
+              <nobr> Prática (3 alunos) </nobr><br /> 
+              <nobr> 4 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 90,00 </nobr> <br />
+              <nobr> Matricula R$ 60,00 </nobr> 
+            </div>
+            <div className='Cursos2'>
+              Pratico II <br /><br />
+              <nobr> Prática (individual) </nobr><br /> 
+              <nobr> 4 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 150,00 </nobr> <br /> 
+              <nobr> Matricula R$ 100,00 </nobr>  
+            </div>
+            <div className='Cursos2'>
+              Teorico I <br /><br />
+              <nobr> Teórica (3 alunos) </nobr><br /> 
+              <nobr> 4 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 90,00 </nobr> <br /> 
+              <nobr>  Matricula R$ 60,00 </nobr>  
+            </div>
+            <div className='Cursos2'>
+              Teorico II <br /><br />
+              <nobr> Teórica (individual)</nobr> <br /> 
+              <nobr> 4 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 150,00 </nobr> <br /> 
+              <nobr> Matricula R$ 100,00 </nobr>  
+            </div>
+            </div>
+            <div className='planos2'> 
+            <div className='Cursos2'>
+              <nobr>Completo I</nobr> <br /><br />
+              <nobr> Teórica (individual) e prática</nobr> <br />
+              <nobr> 4 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 200,00 </nobr> <br /> 
+              <nobr> Matricula R$ 130,00</nobr>  
+            </div>
+            <div className='Cursos2'>
+              <nobr>Completo II</nobr> <br /><br />
+              <nobr> Teórica (3 alunos) e prática</nobr> <br />
+              <nobr> 8 aulas/mês</nobr> <br /> 
+              <nobr> Mensalidade R$ 300,00 </nobr> <br /> 
+              <nobr> Matricula R$ 150,00</nobr> 
+            </div>
+            <div className='Cursos2'>
+              <nobr>Completo III</nobr> <br /> <br /> 
+              <nobr> Teórica (individual) e prática </nobr> <br />
+              <nobr> 8 aulas/mês </nobr> <br />
+              <nobr> Mensalidade R$ 400,00 </nobr> <br />
+              <nobr> Matricula R$ 200,00 </nobr> 
+            </div>
+          </div>
+        </div>
         </Route>
       </div>
     </Router>
