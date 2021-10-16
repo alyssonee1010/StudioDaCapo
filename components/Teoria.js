d26d32c024849919745f48b66d421de45ea50995
Alysson Teixeira <queijos410@gmail.com>
2021-10-16 15:15:43 -0300

    buttons design

--- components/Teoria.js
+++ components/Teoria.js
@@ -6,15 +6,15 @@ function Teoria() {
       <div>
         <Route path='/Teoria' exact component={Teoria}>
           <div id='SBA'> 
-          <div className='titulo sba'> Teoria da Capo <br /><br /> </div>
             <div className='SobreAulas'>
+            <div className='titulo'>Aulas DaCapo<br /><br /></div>
               As aulas são de 50 minutos a 1 hora de duração. Aula individual 
               é dada com professor e Aluno, aula em grupo pode chegar até 3 alunos além do professor.
               As aulas somente práticas são focadas em como tocar o instrumento em 
-              especifico (posicionamento da mão etc), e as aulas teóricas e praticas ajudam 
+              especifico, já as aulas teóricas e praticas ajudam 
               o aluno compreender a música como um todo, existe um limite que o 
               aluno pode chegar sem a teoria. A aula somente teórica é recomendada para alunos que ja tocam
-              e buscam aperfeiçoar a sua compreenssão da música como um todo.
+              e buscam melhorar a sua compreenssão da música.
             </div>
           </div>
         </Route>
