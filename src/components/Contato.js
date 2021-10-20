fb21b3a59baaff6ffb412bc8e88c6f466c031f19
Alysson Teixeira <queijos410@gmail.com>
2021-10-20 15:44:26 -0300

    email fix

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -17,7 +17,7 @@ function Contato() {
             <div className='email'>
             <h1> E-mails </h1>
               Studio DaCapo <cont> dacapo.studioeloja@gmail.com</cont> <br />
-              Alysson <cont>queijos410@gmail.com</cont> <br />
+              Alysson <cont>alyssonteixeiraaaa@gmail.com</cont> <br />
               Diego  <cont>diegosenna1993@gmail.com</cont> <br />
               Luciano <cont>ossunabraza@gmail.com </cont>
             </div>
